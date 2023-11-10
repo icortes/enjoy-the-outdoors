9588e12a504bcf4588a4f59c979a3df1189a4a2c
Isaac Cortes Hernandez <isaaccortes014@gmail.com>
2023-11-09 18:23:15 -0800

    bootstrap, navigation bar, page routes

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+
+export default function NavBar() {
+  return (
+    <nav className='navbar navbar-expand-lg bg-body-tertiary'>
+      <div className='container-fluid'>
+        <Link className='navbar-brand' href='/'>
+          Enjoy the Outdoors
+        </Link>
+        <button
+          className='navbar-toggler'
+          type='button'
+          data-bs-toggle='collapse'
+          data-bs-target='#navbar'
+          aria-controls='navbar'
+          aria-expanded='false'
+          aria-label='Toggle navigation'>
+          <span className='navbar-toggler-icon'></span>
+        </button>
+        <div className='collapse navbar-collapse' id='navbar'>
+          <div className='navbar-nav'>
+            <Link className='nav-link' href='/nationalparks'>
+              National Parks
+            </Link>
+            <Link className='nav-link' href='/mountains'>
+              Mountains
+            </Link>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
