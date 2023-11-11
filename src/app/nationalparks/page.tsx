586cf740e8bb80006fb385c119f498ef0243307f
Isaac Cortes Hernandez <isaaccortes014@gmail.com>
2023-11-10 22:02:18 -0800

    search bar

--- src/app/nationalparks/page.tsx
+++ src/app/nationalparks/page.tsx
@@ -1,26 +1,27 @@
 'use client';
 
-import { SetStateAction, useState } from 'react';
+import { signal } from '@preact/signals';
 import prisma from '../../../prisma/prisma';
 
 export default function NationalParks() {
   // const parks = await prisma.national_park_data.findMany({});
   // console.log(parks);
   const searchOptions = ['by location', 'by park type'];
-  let [searchBy, setSearchBy] = useState('by location');
+  let selectedOption = signal('by location');
 
-  function handleOptionChange(event: {
-    target: { value: SetStateAction<string> };
-  }): void {
-    setSearchBy(event.target.value);
+  function handleOptionChange(event: any): void {
+    selectedOption = event.target.value;
     console.log(event.target.value);
   }
 
   return (
     <main className='container-md'>
-      <div className='input-group'>
-        <span className='input-group-text'>Search by</span>
-        <select className='input-group-text' id='search-option' onChange={handleOptionChange}>
+      <div className='input-group mt-3'>
+        <span className='input-group-text'>Search</span>
+        <select
+          className='input-group-text'
+          id='search-option'
+          onChange={handleOptionChange}>
           {searchOptions.map((_option) => {
             return (
               <option key={_option} value={_option}>
@@ -29,6 +30,12 @@ export default function NationalParks() {
             );
           })}
         </select>
+        <input
+          className='form-control'
+          id='search-input'
+          type='text'
+          placeholder='search park here...'
+        />
       </div>
     </main>
   );
