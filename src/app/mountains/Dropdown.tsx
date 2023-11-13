c4f4e46add48538850f79769f8c9bbdd247801ed
Isaac Cortes Hernandez <isaaccortes014@gmail.com>
2023-11-13 10:11:27 -0800

    dropdown value is stored in global state

--- src/app/mountains/Dropdown.tsx
+++ src/app/mountains/Dropdown.tsx
@@ -1,10 +1,22 @@
+import { useMountainStore } from '../store/SelectedMountainStore';
+
 export default function Dropdown({ mountainNames }: { mountainNames: any[] }) {
+  const mountainStore = useMountainStore();
+
+  function handleOnChange(event: { currentTarget: { value: string } }) {
+    mountainStore.updateMountain(event.currentTarget.value);
+  }
+
   return (
     <div className='form-floating mt-3'>
-      <select className='form-select' id='mountainsSelect' defaultValue={''}>
-        {mountainNames.map((mountain) => {
+      <select
+        className='form-select'
+        id='mountainsSelect'
+        onChange={handleOnChange}
+        value={mountainStore.selectedMountain}>
+        {mountainNames.map((mountain, index) => {
           return (
-            <option key={mountain} value={mountain}>
+            <option key={index} value={mountain}>
               {mountain}
             </option>
           );
