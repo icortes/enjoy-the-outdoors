81e32274b9a74e80dfc0bf13f722443c4669f749
Isaac Cortes Hernandez <isaaccortes014@gmail.com>
2023-11-13 10:23:30 -0800

    added type to mountain data

--- src/app/mountains/page.tsx
+++ src/app/mountains/page.tsx
@@ -3,8 +3,18 @@
 import { useEffect, useState } from 'react';
 import Dropdown from './Dropdown';
 
+type Mountain = {
+  id: number;
+  name: string;
+  effort: string;
+  desc: string;
+  img: string;
+  elevation: number;
+  coords: { lat: number; lng: number };
+};
+
 export default function Mountains() {
-  const [mountainData, setMountainData] = useState<any[]>([]);
+  const [mountainData, setMountainData] = useState<Mountain[]>([]);
   const mountainNames = mountainData.map((mountain) => mountain.name);
 
   useEffect(() => {
