5c3f44dc094f91d37e20c8f44cd79725f7cd593f
Isaac Cortes Hernandez <isaaccortes014@gmail.com>
2023-11-14 11:07:16 -0800

    jumbotron and navbar styling

--- src/app/(home)/Jumbotron.tsx
+++ src/app/(home)/Jumbotron.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 export default function Jumbotron() {
   const jumbo = {
     backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
@@ -5,23 +7,36 @@ export default function Jumbotron() {
     backgroundRepeat: 'no-repeat',
     backgroundSize: 'cover',
     backgroundPosition: 'center center',
-    height: '95vh',
+    height: '100vh',
 
     textShadow: '3px 3px 2px rgba(0, 0, 0, 1)',
   };
-  //TODO: Add 2 buttons for other pages
+
+  const noTextShadow = {
+    textShadow: 'none',
+  };
   return (
-    <div className='p-md-5 mb-md-4 text-md-end text-sm-start' style={jumbo}>
-      <div className='container-fluid h-100 d-flex flex-column justify-content-around py-2 py-sm-3 py-md-5'>
-        <h1 className='display-3 fw-bold'>Enjoy the Outdoors</h1>
-        <p className='col-md-9 fs-4 mt-md-5 text-start'>
+    <div className='pt-5 mb-md-4 text-md-end text-sm-start' style={jumbo}>
+      <div className='container-fluid h-100 d-flex flex-column justify-content-around mt-5 py-3 py-sm-3 py-md-5'>
+        <h1 className='display-3 fw-bold'>SummitQuest</h1>
+        <div className='col-md-9 fs-4 mt-md-5 text-start'>
           Welcome to SummitQuest, your go-to resource for discovering the splendor of
           national parks and conquering majestic mountains! Immerse yourself in the
           wonders of nature as you embark on unforgettable adventures in the great
           outdoors. Whether you&apos;re a seasoned mountaineer or a nature enthusiast
           seeking a tranquil escape, we&apos;ve got the perfect destination waiting for
           you.
-        </p>
+          <div
+            className='mt-5 d-flex flex-column flex-sm-row justify-content-sm-evenly align-items-between'
+            style={noTextShadow}>
+            <Link href={'/nationalparks'} className='btn btn-success fs-4'>
+              National Parks<i className='bi bi-box-arrow-up-right ms-2'></i>
+            </Link>
+            <Link href={'/mountains'} className='btn btn-success fs-4 mt-3 m-sm-0'>
+              Mountains<i className='bi bi-box-arrow-up-right ms-2'></i>
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/app/NavBar.tsx
+++ src/app/NavBar.tsx
@@ -1,11 +1,38 @@
-import Link from "next/link";
+'use client';
+
+import Link from 'next/link';
+import { useState } from 'react';
 
 export default function NavBar() {
+  const [navbar, setNavBar] = useState(false);
+
+  const changeBackground = () => {
+    if (window.scrollY >= 99) {
+      setNavBar(true);
+    } else {
+      setNavBar(false);
+    }
+    //99
+  };
+
+  window.addEventListener('scroll', changeBackground);
+
   return (
-    <nav className='navbar navbar-expand-lg bg-body-tertiary'>
+    <nav
+      className={
+        navbar
+          ? 'navbar navbar-expand-sm bg-body-secondary fixed-top'
+          : 'navbar navbar-expand-sm bg-transparent fixed-top'
+      }>
       <div className='container-fluid'>
         <Link className='navbar-brand' href='/'>
-          SummitQuest
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            src='assets/logo/logo.png'
+            alt='logo'
+            className='d-inline-block align-text-top'
+            width={50}
+          />
         </Link>
         <button
           className='navbar-toggler'
@@ -19,10 +46,13 @@ export default function NavBar() {
         </button>
         <div className='collapse navbar-collapse' id='navbar'>
           <div className='navbar-nav'>
-            <Link className='nav-link' href='/nationalparks'>
+            <Link className='nav-link fs-4 text-white' href='/'>
+              Home
+            </Link>
+            <Link className='nav-link fs-4 text-white' href='/nationalparks'>
               National Parks
             </Link>
-            <Link className='nav-link' href='/mountains'>
+            <Link className='nav-link fs-4 text-white' href='/mountains'>
               Mountains
             </Link>
           </div>
